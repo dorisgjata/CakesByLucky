bc28c102c0bf71160ea1298eddaddd00a29610da
dorisgjata <dorisgjata@oakland.edu>
2018-03-25 23:59:23 -0400

    fixed email and other stuff

---
+++ src/main/java/com/cakes/cake/mvc/CakeController.java
@@ -0,0 +1,25 @@
+package com.cakes.cake.mvc;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CakeController {
+
+    @RequestMapping("/cake")
+    public String cake(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "cake";
+    }
+    
+    @RequestMapping("/contact")
+    public String contact(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        model.addAttribute("email", new SimpleMailMessage());
+
+        return "contact";
+    }
+
+}
